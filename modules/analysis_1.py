c6b179b28b695aeb4a10efd49c3b5ea37655283f
xiaoyu-uyn <787662324@qq.com>
2020-11-29 08:47:36 -0500

    ana 1&2 updates Date: 2020-11-29

--- modules/analysis_1.py
+++ modules/analysis_1.py
@@ -36,76 +36,114 @@ def analysis_1():
                     pre = data[j][0]
                 plt.plot(11-i,plot_y,colors[j])
                 plt.annotate(data[j][1], (11-i, plot_y))
-                max_n = max(max_n,data[j][0])
+            max_n = max(max_n, sum_c)
             _sum.append(sum_c)
         _sum.reverse()
         plt.plot(range(1,11),_sum,'g--', label="top 3 sum")
         plt.legend()
         plt.ylabel('Borrowed Numbers')
-        plt.title('Top borrowed books curve')
+        plt.title('Top borrowed books daily')
         plt.xticks(range(12),('','10 days ago','9','8','7','6','5','4','3','2','1',''))
         plt.yticks(range(0, max_n + 2))
         plt.savefig('static/images/top3booksCurve.jpg')
         plt.clf()
-        plt.figure(figsize=(6, 5))
-        # update topk frequently borrowed books in last week
-        sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 7 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
-        cursor.execute(sql)
-        data = cursor.fetchall()
-        print(len(data))
-        num = []
-        for i in range(len(data)):
-            plt.annotate(data[i][1], (i+1, data[i][0]))
-            num.append(int(data[i][0]))
-        plt.scatter(range(1,4),num,c = 'blue')
-        plt.ylabel('Borrowed Numbers')
-        plt.title('Top 3 frequently borrowed books in last week')
-        plt.xticks(range(0,5))
-        if len(data) > 0:
-            plt.yticks(range(0, data[0][0] + 2))
-        else:
-            plt.yticks(range(0, 2))
-        plt.savefig('static/images/top3booksInWeek.jpg')
-        plt.clf()
-        # update topk frequently borrowed books in last month
-        sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 31 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
-        cursor.execute(sql)
-        data = cursor.fetchall()
-        print(len(data))
-        num = []
-        for i in range(len(data)):
-            plt.annotate(data[i][1], (i+1, data[i][0]))
-            num.append(int(data[i][0]))
-        plt.scatter(range(1,4),num,c = 'blue')
-        plt.ylabel('Borrowed Numbers')
-        plt.title('Top 3 frequently borrowed books in last month')
-        plt.xticks(range(0,5))
-        if len(data) > 0:
-            plt.yticks(range(0, data[0][0] + 2))
-        else:
-            plt.yticks(range(0, 2))
-        plt.savefig('static/images/top3booksInMonth.jpg')
-        plt.clf()
-        # update topk frequently borrowed books in last 1 day
-        sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 1 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
-        cursor.execute(sql)
-        data = cursor.fetchall()
-        print(len(data))
-        num = []
-        for i in range(len(data)):
-            plt.annotate(data[i][1], (i+1, data[i][0]))
-            num.append(int(data[i][0]))
-        plt.scatter(range(1,len(num)+1),num,c = 'blue')
-        plt.ylabel('Borrowed Numbers')
-        plt.title('Top 3 frequently borrowed books in last day')
-        plt.xticks(range(0,5))
-        if len(data) > 0:
-            plt.yticks(range(0, data[0][0] + 2))
-        else:
-            plt.yticks(range(0, 2))
 
-        plt.savefig('static/images/top3booksInDay.jpg')
+        # update daily data in last 12 months(top3)
+        max_n = 0
+        _sum = []
+        plt.figure(figsize=(12, 11))
+        for i in range(1,13):
+            sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and months_between(sysdate,request_start) <= :1 and months_between(sysdate,request_start) >= :2 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
+            val = [i,i-1]
+            cursor.execute(sql, val)
+            data = cursor.fetchall()
+            pre = -1
+            counter = 0.25
+            sum_c = 0
+            for j in range(len(data)):
+                plot_y = data[j][0]
+                sum_c += data[j][0]
+                if pre == plot_y:
+                    plot_y -= counter
+                    counter += 0.25
+                else:
+                    counter = 0.25
+                    pre = data[j][0]
+                plt.plot(13-i,plot_y,colors[j])
+                plt.annotate(data[j][1], (13-i, plot_y))
+            max_n = max(max_n, sum_c)
+            _sum.append(sum_c)
+        _sum.reverse()
+        plt.plot(range(1,13),_sum,'g--', label="top 3 sum")
+        plt.legend()
+        plt.ylabel('Borrowed Numbers')
+        plt.title('Top borrowed books monthly')
+        plt.xticks(range(14),('','12 months ago','11','10','9','8','7','6','5','4','3','2','1',''))
+        plt.yticks(range(0, max_n + 2))
+        plt.savefig('static/images/top3booksCurveMonthly.jpg')
         plt.clf()
+
+
+
+        # plt.figure(figsize=(6, 5))
+        # # update topk frequently borrowed books in last week
+        # sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 7 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
+        # cursor.execute(sql)
+        # data = cursor.fetchall()
+        # print(len(data))
+        # num = []
+        # for i in range(len(data)):
+        #     plt.annotate(data[i][1], (i+1, data[i][0]))
+        #     num.append(int(data[i][0]))
+        # plt.scatter(range(1,4),num,c = 'blue')
+        # plt.ylabel('Borrowed Numbers')
+        # plt.title('Top 3 frequently borrowed books in last week')
+        # plt.xticks(range(0,5))
+        # if len(data) > 0:
+        #     plt.yticks(range(0, data[0][0] + 2))
+        # else:
+        #     plt.yticks(range(0, 2))
+        # plt.savefig('static/images/top3booksInWeek.jpg')
+        # plt.clf()
+        # # update topk frequently borrowed books in last month
+        # sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 31 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
+        # cursor.execute(sql)
+        # data = cursor.fetchall()
+        # print(len(data))
+        # num = []
+        # for i in range(len(data)):
+        #     plt.annotate(data[i][1], (i+1, data[i][0]))
+        #     num.append(int(data[i][0]))
+        # plt.scatter(range(1,4),num,c = 'blue')
+        # plt.ylabel('Borrowed Numbers')
+        # plt.title('Top 3 frequently borrowed books in last month')
+        # plt.xticks(range(0,5))
+        # if len(data) > 0:
+        #     plt.yticks(range(0, data[0][0] + 2))
+        # else:
+        #     plt.yticks(range(0, 2))
+        # plt.savefig('static/images/top3booksInMonth.jpg')
+        # plt.clf()
+        # # update topk frequently borrowed books in last 1 day
+        # sql = "select count(request_id),book_name,book_id from customer_request where request_status <> 'W' and sysdate - request_start <= 1 group by book_id, book_name order by 1 desc OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY"
+        # cursor.execute(sql)
+        # data = cursor.fetchall()
+        # print(len(data))
+        # num = []
+        # for i in range(len(data)):
+        #     plt.annotate(data[i][1], (i+1, data[i][0]))
+        #     num.append(int(data[i][0]))
+        # plt.scatter(range(1,len(num)+1),num,c = 'blue')
+        # plt.ylabel('Borrowed Numbers')
+        # plt.title('Top 3 frequently borrowed books in last day')
+        # plt.xticks(range(0,5))
+        # if len(data) > 0:
+        #     plt.yticks(range(0, data[0][0] + 2))
+        # else:
+        #     plt.yticks(range(0, 2))
+        #
+        # plt.savefig('static/images/top3booksInDay.jpg')
+        # plt.clf()
 
 
         plt.close()
@@ -113,9 +151,12 @@ def analysis_1():
         cursor.close()
         con.close()
 
-    image_name1 = "/static/images/top3booksInDay.jpg?" + str(time.time())
-    image_name2 = "/static/images/top3booksInWeek.jpg?" + str(time.time())
-    image_name3 = "/static/images/top3booksInMonth.jpg?" + str(time.time())
+    # image_name1 = "/static/images/top3booksInDay.jpg?" + str(time.time())
+    # image_name2 = "/static/images/top3booksInWeek.jpg?" + str(time.time())
+    # image_name3 = "/static/images/top3booksInMonth.jpg?" + str(time.time())
     image_name4 = "/static/images/top3booksCurve.jpg?" + str(time.time())
+    image_name5 = "/static/images/top3booksCurveMonthly.jpg?" + str(time.time())
     # show three figures
-    return render_template('analysis_1.html', image_name1=image_name1, image_name2=image_name2, image_name3=image_name3, image_name4=image_name4)
+    return render_template('analysis_1.html', image_name5=image_name5,
+                           image_name4=image_name4)
+    # return render_template('analysis_1.html', image_name1=image_name1, image_name2=image_name2, image_name3=image_name3, image_name4=image_name4)
